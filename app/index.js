c47d0c8cb0c96ce3299ce466467a1041e2838691
Chris Auret <chris.auret@yahoo.co.uk>
2016-04-30 00:53:03 +1000

    Display video results in list

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 
-const API_KEY = '';
+const API_KEY = 'your youtube key';
 
 class App extends Component {
     constructor(props) {
